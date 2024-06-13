4dde2456fc39642831efcae0287f55e2679d3ff9
Hunter <hunterde91@gmail.com>
2024-06-13 18:47:09 -0400

    added the schema.sql

--- server.js
+++ server.js
@@ -208,6 +208,60 @@ function updateEmployeeRole() {
         });
     });
 }
+
+function promptUser() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit',
+                ],
+            },
+        ])
+        .then((answer) => {
+            switch (answer.choice) {
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Exit':
+                    client.end();
+                    break;
+                default:
+                    break;
+            }
+        });
+}
+
+promptUser();
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
