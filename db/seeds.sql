0e8ccc340e3aebd4021d66ae7f7f935d0cdf898d
Hunter <hunterde91@gmail.com>
2024-06-13 19:04:19 -0400

    added seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,10 @@
 INSERT INTO departments (name)
 VALUES ('HR'),('Management'),('Finance'),('Customer Service')
 
-INSERT INTO roles (title, )
-VALUES ('HR'),('Management'),('Finance'),('Customer Service')
+INSERT INTO roles (title, salary, department_id )
+VALUES  ('HR Manager', 100000.00, 2),
+        ('CEO', 250000.00, 2),
+        ('CFO', 150000.00, 2),
+        ('Employee Rep', 75000.00, 1)
+        ('Accountant', 85000.00, 3)
+        ('Representative', 45000.00, 4)
